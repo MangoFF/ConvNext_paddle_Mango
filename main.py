8d668861df81f17cc4edde6b429338096bfc2941
mango <939117440@qq.com>
2022-04-12 10:48:18 +0800

    merge writer's code

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def str2bool(v):
 
 def get_args_parser():
     parser = argparse.ArgumentParser('ConvNeXt training and evaluation script for image classification', add_help=False)
-    parser.add_argument('--batch_size', default=64, type=int,
+    parser.add_argument('--batch_size', default=1, type=int,
                         help='Per GPU batch size')
     parser.add_argument('--epochs', default=300, type=int)
     parser.add_argument('--update_freq', default=1, type=int,
